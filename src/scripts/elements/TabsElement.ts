3518cce0ddc7430ded7771de814aadf39370bdaf
Parth Khatri <p.khatri21@gmail.com>
2023-12-27 21:46:31 +1100

    Minor tabs HTML update

--- src/scripts/elements/TabsElement.ts
+++ src/scripts/elements/TabsElement.ts
@@ -7,28 +7,26 @@ export default (data:Array<dataItem>) => {
   const titles = document.createElement('div');
   titles.classList.add('tabs__titles');
 
+  const contents = document.createElement('div');
+  contents.classList.add('tabs__contents');
+
   data.forEach((item:dataItem, index:number) => {
     const title = document.createElement('div');
     title.setAttribute('data-tab', '');
     title.classList.add('tabs__title');
     if(index === 0) title.classList.add('is-active');
 
-    title.innerHTML = `
-      <span class="tabs__title-inner">${item.title}</span>
-    `;
-    
+    const titleInner = document.createElement('span');
+    title.classList.add('tabs__title-inner');
+    titleInner.innerHTML = item.title;
+    title.appendChild(titleInner);
+
     titles.appendChild(title);
-  });
 
-  const contents = document.createElement('div');
-  contents.classList.add('tabs__contents');
-
-  data.forEach((item:dataItem, index:number) => {
     const content = document.createElement('div');
     content.setAttribute('data-content', '');
     content.classList.add('tabs__content');
     if(index === 0) content.classList.add('is-active');
-
     content.innerHTML = item.content;
     
     contents.appendChild(content);
